2aea863ffc81e773e332f2c13f8d2269efd0fc5a
Vinicius Feitosa da Silva <oviniciusfeitosa@gmail.com>
2024-08-20 01:01:44 -0300

    ✨ Add convex sample

--- pocs/nextjs-convex-clerk-auth/convex/_generated/api.d.ts
+++ pocs/nextjs-convex-clerk-auth/convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as myFunctions from "../myFunctions.js";
 import type * as tasks from "../tasks.js";
 
 /**
@@ -25,6 +26,7 @@ import type * as tasks from "../tasks.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  myFunctions: typeof myFunctions;
   tasks: typeof tasks;
 }>;
 export declare const api: FilterApi<
---
+++ pocs/nextjs-convex-clerk-auth/src/app/ConvexClientProvider.tsx
@@ -0,0 +1,34 @@
+// "use client";
+
+// import { ConvexProvider, ConvexReactClient } from "convex/react";
+// import { ReactNode } from "react";
+
+// const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+// export function ConvexClientProvider({ children }: { children: ReactNode }) {
+//   return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+// }
+
+"use client";
+
+import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ErrorBoundary } from "./ErrorBoundary";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export function ConvexClientProvider({ children }: { children: ReactNode }) {
+  return (
+    <ErrorBoundary>
+      <ClerkProvider
+        publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+      >
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          {children}
+        </ConvexProviderWithClerk>
+      </ClerkProvider>
+    </ErrorBoundary>
+  );
+}
